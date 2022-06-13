c5811f4dca7c1634a880017286bdcb3a8486f309
Johannes Langosch <johannes.langosch@googlemail.com>
2022-06-13 19:33:38 +0200

    05a change Post bg color

--- pages/components/Post.js
+++ pages/components/Post.js
@@ -2,7 +2,7 @@ import timeago from "lib/timeago";
 
 export default function Post({ post }) {
   return (
-    <div className="flex flex-col mb-4 border rounded-xl shadow-xl border-3 border-black p-10 bg-gray-200 mx-20 my-10">
+    <div className="flex flex-col mb-4 border bg-blue-200 rounded-xl shadow-xl border-3 border-black p-10 mx-20 my-10">
       <div className="flex flex-shrink-0 pb-0 ">
         <div className="flex-shrink-0 block group ">
           <div className="flex items-center text-gray-800">
